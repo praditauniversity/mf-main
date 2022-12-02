a3606abb2917c8326ddd4e09241cd3dfbc3b4cc7
Patricia Ho <hopatricia1711@gmail.com>
2022-12-02 12:56:12 +0700

    tailwind solor style

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,59 @@ module.exports = {
         'secondary-secondary': '#90CAF9',
         'secondary-light': '#E3F2FD',
 
+        'tertiary': '#8AB73A',
+        'tertiary-800': '#82A027',
+        'tertiary-dark': '#88B135',
+        'tertiary-light': '#F0F6E7',
+        'tertiary-tertiary': '#C5DB9D',
+
+        'grey': '#8B8F9F',
+        'grey-light': '#ACAFBF',
+        'grey-lighter': '#CCD1D7',
+        'grey-border': '#ECEEF0',
+        'grey-dark': '#7D8292',
+        'grey-lite': '#2A4157',
+        'grey-icon': '#33363F',
+        
+        'error': '#DA4747',
+        'error-light': '#DD7474',
+        'error-dark': '#C62828',
+
+        'orange': '#FDAC42',
+        'orange-light': '#FCCC75',
+        'orange-lighter': '#FEF0D6',
+        'orange-dark': '#FF8800',
+        'orange-darker': '#E54C00',
+
+        'text-dark': '#252B4B',
+        'text-subtitle': '#505F73',
+        'text-normal': '#475467',
+        'text-light': '#A3AED0',
+        'text-lighter': '#CCD1D7',
+        'text-lite': '#9EA9CB',
+        'text-snow': '#FAFAFA',
+
+        'table-dark': '#F5F6FA',
+        'table-light': '#E9EDF7',
+
+        'black': '#101828',
+        'black-light': '#161E2E',
+        
+        'warning': '#FDDD48',
+        'warning-light': '#FDED72',
+        'warning-dark': '#FFC107',
+
+        'teal': '#73DFE7',
+        'teal-light': '#A9EFF2',
+        'teal-dark': '#00CFDE',
+
+        'tag-text': '#505F73',
+        'tag-bg': '#EEEEEE',
+
+        'gantt-border': '#EBEBF0',
+        'gantt-stripe-light': '#FAFAFC',
+        'gantt-stripe-dark': '#EAEEF4',
+
       },
 
       gridTemplateColumns: {
