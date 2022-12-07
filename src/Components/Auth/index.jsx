e5e48f4bab4b1d8b3ea2ba3a098f76988cebba0f
AustinNick <46616395+AustinNick@users.noreply.github.com>
2022-12-07 22:26:56 +0700

    refactoring (delete unused text)

--- src/Components/Auth/index.jsx
+++ src/Components/Auth/index.jsx
@@ -13,12 +13,11 @@ const Logout = () => {
 
     return (
         <div className=" max-w-sm mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300 sm:max-w-lg ">
-            <p className="font-bold text-md tracking-widest uppercase">Logout</p>
+            <p className="font-bold text-xl tracking-widest uppercase">Logout</p>
             <p className="mb-8 italic text-sm">Logout from the system.</p>
-            <p className="text-md ">Welcome to the System.</p>
             <form onSubmit={handleSubmit}>
                 <div className="py-4 mx-auto flex items-center justify-between space-x-4">
-                <Button buttontype="submit" label="Logout" />
+                    <Button buttontype="submit" label="Logout" />
                 </div>
             </form>
         </div>
