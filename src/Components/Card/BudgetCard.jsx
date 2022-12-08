dc7108454c1c687db20510630ff0d51f12ffef2c
PhanceK <phance01@gmail.com>
2022-12-08 14:18:26 +0700

    update icon in budgetcard

--- src/Components/Card/BudgetCard.jsx
+++ src/Components/Card/BudgetCard.jsx
@@ -5,14 +5,14 @@ import { faDollar } from "@fortawesome/free-solid-svg-icons";
 const BudgetCard = (props) => {
   const { icon, title, description, content, colorIcon } = props;
   return (
-    <div className="bg-white flex justify-between mx-auto items-start align-middle flex-row rounded-lg h-32">
+    <div className="bg-white flex justify-between mx-auto items-start align-middle flex-row h-32">
       <div className="py-4 px-4">
         <p className="py-1 text-md">{title}</p>
         <p className="py-1 font-semibold text-md">{description}</p>
         <p className="py-1 opacity-70 text-sm">{content}</p>
       </div>
-      <div className={`py-4 px-4 my-auto ${colorIcon}`}>
-        <FontAwesomeIcon icon={faDollar} size="4x" />
+      <div className={`border-8 py-4 px-4 bg-red-400 m-auto w-24 h-24 text-center items-center ${colorIcon}`} style={{ borderRadius: "9999px" }}>
+        <FontAwesomeIcon classname="text-center align-middle items-center content-center" icon={faDollar} size="4x" />
       </div>
     </div>
   );
