9d4fe06cb05fa6ff3529fb447db6030fbf931160
lipaw08 <100027789+lipaw08@users.noreply.github.com>
2022-12-08 16:22:50 +0700

    Project Dashboard  Layout

--- src/Pages/ProjectDashboard/index.jsx
+++ src/Pages/ProjectDashboard/index.jsx
@@ -10,7 +10,7 @@ const ProjectDashboardPage = () => {
     return (
         <div className="grid grid-cols-3 gap-2 sm:grid-cols-6 lg:grid-cols-9 xl:grid-cols-12 2xl:grid-cols-18 lg:mt-0 mt-4">
             {/* row pertama col pertama */}
-            <div className="col-span-9"> 
+            <div className="col-span-9 row-span-1"> 
             <ProjectOverviewCard 
             title1="Project Name" 
             description1="Project Anomaly" 
@@ -19,17 +19,17 @@ const ProjectDashboardPage = () => {
             title3="Customer" 
             description3="Jaya Gedung Group" 
             /></div>
-            <div className="col-span-3"> <HealthCard title="Health by Cost" description="Cost Overrun" colorIcon="text-error-dark" /> </div>
-            <div className="col-span-3"> <HealthCard title="Health by Schedule" description="Early Schedule" colorIcon="text-tertiary-dark" /> </div>
+            <div className="col-span-3 row-span-1"> <HealthCard title="Health by Cost" description="Cost Overrun" colorIcon="text-error-dark" /> </div>
+            <div className="col-span-3 row-span-1"> <HealthCard title="Health by Schedule" description="Early Schedule" colorIcon="text-tertiary-dark" /> </div>
             {/* should be calendar | row pertama col kedua */}
-            <div className="col-span-3"> <UserCard /> </div>
+            <div className="col-span-3 row-span-3"> <UserCard /> </div>
             {/* should be row kedua col pertama */}
-            <div className="col-span-3"> <BudgetCard title="Budget" description="IDR 42.562.347" content="IDR 816.204.031" colorIcon="text-secondary-800" /> </div>
-            <div className="col-span-3"> <BudgetCard title="Actual" description="IDR 42.562.347" content="IDR 816.204.031" colorIcon="text-primary-800" /> </div>
-            <div className="col-span-3"> <BudgetCard title="Cost" description="IDR 42.562.347" content="IDR 816.204.031" colorIcon="text-orange-dark" /> </div>
-            <div className="col-span-3"> <BudgetCard title="Danger" description="IDR 42.562.347" content="IDR 816.204.031" colorIcon="text-error-dark" /> </div>
-            <div className="col-span-3"> <BudgetCard title="Variance" description="IDR 42.562.347" content="IDR 816.204.031" colorIcon="text-tertiary-800" /> </div>
-            <div className="col-span-6"> <ApexRedialBarChart /> </div>
+            <div className="col-span-3 row-span-1"> <BudgetCard title="Budget" description="IDR 42.562.347" content="IDR 816.204.031" colorIcon="text-secondary-800" /> </div>
+            <div className="col-span-3 row-span-1"> <BudgetCard title="Actual" description="IDR 42.562.347" content="IDR 816.204.031" colorIcon="text-primary-800" /> </div>
+            <div className="col-span-3 row-span-1"> <BudgetCard title="Cost" description="IDR 42.562.347" content="IDR 816.204.031" colorIcon="text-orange-dark" /> </div>
+            <div className="col-span-3 row-span-1"> <BudgetCard title="Danger" description="IDR 42.562.347" content="IDR 816.204.031" colorIcon="text-error-dark" /> </div>
+            <div className="col-span-3 row-span-1"> <BudgetCard title="Variance" description="IDR 42.562.347" content="IDR 816.204.031" colorIcon="text-tertiary-800" /> </div>
+            <div className="col-span-6 row-span-1"> <ApexRedialBarChart /> </div>
         </div>
     );
 }
