9256623d7a16ee3f64477643e66a82439b283cd8
archaict <crosley40@gmail.com>
2022-12-08 09:26:42 +0000

    Update PMODashboard

--- src/Components/Card/BudgetCard.jsx
+++ src/Components/Card/BudgetCard.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDollar } from "@fortawesome/free-solid-svg-icons";
 
 const BudgetCard = (props) => {
-  const { icon, title, description, content, colorIcon } = props;
+  const { icon, title, description, content, colorIcon, borderColor } = props;
   return (
     <div className="bg-white flex justify-between mx-auto items-start align-middle flex-row h-32">
       <div className="py-4 px-4">
@@ -11,8 +11,10 @@ const BudgetCard = (props) => {
         <p className="py-1 font-semibold text-md">{description}</p>
         <p className="py-1 opacity-70 text-sm">{content}</p>
       </div>
-      <div className={`border-8 py-4 px-4 bg-red-400 m-auto w-24 h-24 text-center items-center ${colorIcon}`} style={{ borderRadius: "9999px" }}>
-        <FontAwesomeIcon classname="text-center align-middle items-center content-center" icon={faDollar} size="4x" />
+      <div className={`border-8 ${borderColor} py-4 px-4 bg-red-400 m-auto w-24 h-24 text-center opacity items-center ${colorIcon}`} style={{ borderRadius: "9999px" }}>
+      {/* <i className={`py-4 px-4 bg-red-400 m-auto w-22 h-22 text-center items-center ${colorIcon}`} style={{ borderRadius: "9999px" }}> */}
+        <FontAwesomeIcon classname="text-center align-middle items-center content-center" icon={faDollar} size="3x" />
+      {/* </i> */}
       </div>
     </div>
   );
