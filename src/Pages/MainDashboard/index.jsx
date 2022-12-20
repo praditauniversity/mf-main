41cd7da4ae449248e49d3239025378a388b8bcb7
archaict <crosley40@gmail.com>
2022-12-20 16:42:00 +0000

    add margin-right only

--- src/Pages/MainDashboard/index.jsx
+++ src/Pages/MainDashboard/index.jsx
@@ -11,7 +11,7 @@ const MainDashboard = () => {
     return (
         <div className="grid grid-cols-3 gap-2 sm:grid-cols-6 lg:grid-cols-9 xl:grid-cols-12 2xl:grid-cols-18 lg:mt-0 mt-2">
             <div className="col-span-12">
-                <div className="col-span-12 ml-5 mb-10">
+                <div className="col-span-12 ml-5 mb-10 mr-5">
                     <div className="flex items-center mb-3">
                         <h3 className="font-bold text-2xl mt-3">On-going Project</h3>
                         <div className="border border-none rounded-full bg-base-100 bg-primary ml-[20px] mt-3 flex items-center p-[5px] pr-[14px] pl-[14px]">
@@ -32,7 +32,7 @@ const MainDashboard = () => {
                     
                 </div>
 
-                <div className="col-span-12 ml-5">
+                <div className="col-span-12 ml-5 mr-5">
                     <h3 className="font-bold text-2xl mt-5 mb-5">Remaining Task</h3>
                     <div className="mt-2 border border-none rounded-lg bg-base-100 bg-background-snow p-5 h-[320px]">
 
