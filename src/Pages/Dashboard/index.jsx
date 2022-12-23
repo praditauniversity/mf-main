f1e4cb7214d668931c7e441aa374e2772b5c933a
AustinNick <46616395+AustinNick@users.noreply.github.com>
2022-12-23 14:34:15 +0700

    insert dashboard content

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import UserCard from "../../Components/Card/UserCard";
-import BlankCard from "../../Components/Card/Blank";
-// import Dropdown from "../../Components/Dropdown";
-import PieChart from "../../Components/Charts/PieChart";
-import DonutChart from "../../Components/Charts/PieChart";
+import CalendarCard from "../../Components/Card/Calendar/CalendarCard";
+import AppGantt from "../../Components/Gantt-Component/AppGantt";
+import ActiveProjectCard from "../../Components/Card/ActiveProject/ActiveProjectCard";
 
 const DashboardPage = () => {
     return (
         <div className="grid grid-cols-3 gap-2 sm:grid-cols-6 lg:grid-cols-9 xl:grid-cols-12 2xl:grid-cols-18 lg:mt-0 mt-2">
-            <div className="col-span-3"> <UserCard /> </div>
-            <div className="col-span-3"> <BlankCard /> </div>
-            <div className="col-span-3"> <BlankCard /> </div>
-            <div className="col-span-3"> <BlankCard /> </div>
-            <div className="col-span-3"> <BlankCard /> </div>
-            <div className="col-span-3"> <BlankCard /> </div>
-            {/* <Dropdown /> */}
-            <div className="col"> <DonutChart /> </div>
-            <div className="col"> <DonutChart /> </div>
+            <div className="col-span-14">
+                <div className="grid grid-cols-14 gap-2">
+                    <div className="col-span-4"> <CalendarCard /> </div>
+                    <div className="col-span-10"> <AppGantt className="h-1/2" title="Gantt Chart" />  </div>
+
+
+                </div>
+            </div>
+
+            <div className="col-span-4 row-span-1">
+                <div className="grid grid-cols-4 gap-2">
+                    <div className="col-span-4">
+                        <UserCard />
+                    </div>
+                    <div className="col-span-4">
+                        <ActiveProjectCard />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -95,13 +95,16 @@ module.exports = {
       },
 
       gridTemplateColumns: {
+        '14': 'repeat(14, minmax(0, 1fr))',
         '15': 'repeat(15, minmax(0, 1fr))',
         '16': 'repeat(16, minmax(0, 1fr))',
         '18': 'repeat(18, minmax(0, 1fr))'
       },
 
       gridColumn: {
+        'span-14': 'span 14 / span 14',
         'span-15': 'span 15 / span 15',
+        'span-18': 'span 18 / span 18',
       }
     },
   },
