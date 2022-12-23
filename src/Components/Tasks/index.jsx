82f70a0daa11c60d53a77d77b0cac78eda45d6ae
archaict <crosley40@gmail.com>
2022-12-23 07:30:04 +0000

    transition border for task component

--- src/Components/Tasks/index.jsx
+++ src/Components/Tasks/index.jsx
@@ -4,7 +4,7 @@ const Tasks = (props) => {
     const { id, icon, projectName, taskName, date } = props;
     return (
         <div
-        className="flex flex-row justify-between align-center rounded-lg bg-background-mainframe border-primary hover:bg-table-light hover:border-l-8 active:bg-table-dark focus:bg-table-light mb-2 p-5 transition ease-out duration-100"
+        className="flex flex-row justify-between align-center rounded-lg bg-background-mainframe border-primary hover:bg-table-light hover:border-l-8 active:bg-table-dark focus:bg-table-light mb-2 p-5 transition-all ease-out duration-100"
         >
             <div className="flex flex-col">
                 <h6 className="text-sm ">{projectName}</h6>
@@ -12,7 +12,7 @@ const Tasks = (props) => {
                 <h6 className="text-xs text-grey ">{date}</h6>
             </div>
 
-            <a href="#" key={id} className="flex justify-center align-center opacity-70 hover:opacity-100 transition ease-out duration-100">
+            <a href="#" key={id} className="flex justify-center align-center opacity-70 hover:opacity-100 ease-out duration-100">
                 <img src={icon} className="w-12"></img>
             </a>
             
