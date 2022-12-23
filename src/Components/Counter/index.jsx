839bac10df0c6aa83a567e73ba9b66b3ff417f14
archaict <crosley40@gmail.com>
2022-12-23 08:14:49 +0000

    counter change font weight

--- src/Components/Counter/index.jsx
+++ src/Components/Counter/index.jsx
@@ -4,7 +4,7 @@ const Counter = (props) => {
     const { value, textColor, bgColor } = props;
     return (
         <div className={"border border-none rounded-full bg-base-100 bg-" + bgColor + " ml-[20px] flex items-center justify-center p-[5px] pr-[14px] pl-[14px] w-10 h-10"}>
-            <h6 className={"text-bold text-lg text-" + textColor}>
+            <h6 className={"font-semibold text-lg text-" + textColor}>
                 {value}
             </h6>
         </div>
