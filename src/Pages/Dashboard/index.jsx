0739a349a3b88eec889d2207db32ee4c6fde1132
AustinNick <46616395+AustinNick@users.noreply.github.com>
2022-12-23 16:39:08 +0700

    fix dashboard design

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -3,16 +3,21 @@ import UserCard from "../../Components/Card/UserCard";
 import CalendarCard from "../../Components/Card/Calendar/CalendarCard";
 import AppGantt from "../../Components/Gantt-Component/AppGantt";
 import ActiveProjectCard from "../../Components/Card/ActiveProject/ActiveProjectCard";
+import ProjectProgressOverviewCard from "../../Components/ApexCharts/ProjectProgressOverview";
+import TaskOverviewCard from "../../Components/ApexCharts/TaskOverview";
+import MemberTaskCard from "../../Components/Card/MemberTask/MemberTaskCard";
 
 const DashboardPage = () => {
     return (
         <div className="grid grid-cols-3 gap-2 sm:grid-cols-6 lg:grid-cols-9 xl:grid-cols-12 2xl:grid-cols-18 lg:mt-0 mt-2">
             <div className="col-span-14">
                 <div className="grid grid-cols-14 gap-2">
-                    <div className="col-span-4"> <CalendarCard /> </div>
-                    <div className="col-span-10"> <AppGantt className="h-1/2" title="Gantt Chart" />  </div>
+                    <div className="col-span-4 row-span-1"> <CalendarCard /> </div>
+                    <div className="col-span-10 row-span-2"> <AppGantt  title="Gantt Chart" />  </div>
+                    <div className="col-span-4 row-span-1"> <TaskOverviewCard />  </div>
 
 
+                    <div className="col-span-full"> <MemberTaskCard /> </div>
                 </div>
             </div>
 
@@ -26,6 +31,8 @@ const DashboardPage = () => {
                     </div>
                 </div>
             </div>
+            
+            <div className="col-span-full"> <ProjectProgressOverviewCard /> </div>
         </div>
     );
 }
